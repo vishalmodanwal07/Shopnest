9505dff2943eff5915c8c7a2fbe794b77e61ce22
vishal modanwal <vk0764394@gmail.com>
2024-07-23 12:12:45 +0530

    redux toolkit setup

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,11 @@
+
+
+function CartItem() {
+  return (
+    <div>
+      CartItem
+    </div>
+  )
+}
+
+export default CartItem;
---
+++ src/components/Products.jsx
@@ -0,0 +1,11 @@
+
+
+function Products() {
+  return (
+    <div>
+      product
+    </div>
+  )
+}
+
+export default Products;
