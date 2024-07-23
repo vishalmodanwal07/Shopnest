446a26cf6c1fa3e3e4506c2a9f41bc17eab60dc1
vishal modanwal <vk0764394@gmail.com>
2024-07-23 12:12:18 +0530

    redux toolkit setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import {Provider} from "react-redux"
+import {store} from './components/redux/Store'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <BrowserRouter>
-
-  <App/>
+  <Provider store ={store}>
+      <App/>
+  </Provider>
+ 
 
  </BrowserRouter>
 );
