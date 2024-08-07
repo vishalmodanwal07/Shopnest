fd2d35c36ac65451ab4f01daa017227a065655f9
vishal modanwal <vk0764394@gmail.com>
2024-08-08 02:49:17 +0530

    changes

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -76,10 +76,7 @@ function Signin() {
                     {' '}
                     Password{' '}
                   </label>
-                  <a href="#" title="" className="text-sm font-semibold text-black hover:underline" style={{ fontFamily: '"Raleway", sans-serif' }}>
-                    {' '}
-                    Forgot password?{' '}
-                  </a>
+                 
                 </div>
                 <div className="mt-2  bg-white dark:bg-gray-800 text-black dark:text-white ">
                   <input
@@ -104,26 +101,7 @@ function Signin() {
               </div>
             </div>
           </form>
-          <div className="mt-3 space-y-3  bg-white dark:bg-gray-800 text-black dark:text-white " style={{ fontFamily: '"Raleway", sans-serif' }}>
-            <button
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none  bg-white dark:bg-gray-800 text-black dark:text-white " style={{ fontFamily: '"Raleway", sans-serif' }}
-            >
-              <span className="mr-2 inline-block  bg-white dark:bg-gray-800 text-black dark:text-white ">
-                <svg
-                  className="h-6 w-6 text-rose-500  bg-white dark:bg-gray-800  dark:text-white "
-                  style={{ fontFamily: '"Raleway", sans-serif' }}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                </svg>
-              </span>
-              Sign in with Google
-            </button>
-            
-          </div>
+          
         </div>
       </div>
     
